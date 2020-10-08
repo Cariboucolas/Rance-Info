bf4bf9b739e69b1af7fcff1c2355e742e38a8552
adtautavel <adtautavel@yahoo.fr>
2020-10-08 13:18:53 +0200

    sort button ok

--- cards.js
+++ cards.js
@@ -1,25 +1,35 @@
-window.onload = function()
+/*window.onload = function()
 {
-    console.log("loaded");
+    console.log("loaded");*/
     const layoutButton = document.querySelector("#layout-button");
+    
     /**
     *card button's actions:
     */
-    layoutButton.addEventListener("click", function () {
-        document.querySelector("#article-cards").classList.toggle("d-none");
+    let cardDeckArray = document.querySelectorAll(".card-deck");
+    layoutButton.addEventListener("click", function () 
+    {
+        
+        for(i =0 ; i<cardDeckArray.length ; i++)
+           {
+            cardDeckArray[i].classList.toggle("d-none");
+            }
+        ;
         document.querySelector("#article-list").classList.toggle("d-none");
     });
 
     /**
     * list button's actions
     */
-}
+
 
 /**
  * Event pour changer l'icône du bouton
  */
-layoutButton.addEventListener("click", function () {
-    let fontAwesomeIcon = layoutButton.querySelector("i");
-    fontAwesomeIcon.classList.toggle("fa-clone");
-    fontAwesomeIcon.classList.toggle("fa-list");
-});
+    layoutButton.addEventListener("click", function () 
+        {
+        let fontAwesomeIcon = layoutButton.querySelector("i");
+        fontAwesomeIcon.classList.toggle("fa-clone");
+        fontAwesomeIcon.classList.toggle("fa-list");
+        });
+//}
